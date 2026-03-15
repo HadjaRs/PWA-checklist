5314c1332bddf5439b9a5ca9aabe7632e4f14d31
HadjaRs <s.hadjareis@gmail.com>
2026-03-15 13:03:21 -0400

    add localStorage

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-let entrada = document.getElementById('entrada')
-let add = document.getElementById('add')
-let lista = document.getElementById('lista')
+let entrada = document.getElementById('entrada');
+let lista = document.getElementById('lista');
 
-function adicionar(){
-    let input = entrada.value
-    if(input.trim() === ""){
-        alert("Escreva algo!")
-        return
+
+window.onload = () => {
+    const dadosSalvos = localStorage.getItem('minhaListaHTML');
+    if (dadosSalvos) {
+        lista.innerHTML = dadosSalvos;
     }
-    let item = '<li onclick="this.classList.toggle(\'feito\')">'+ input + '</li>'
-    lista.innerHTML = lista.innerHTML + item
-    entrada.value = ""
+};
+
+
+function atualizarStorage() {
+    localStorage.setItem('minhaListaHTML', lista.innerHTML);
 }
 
-function limpar(){
-    lista.innerHTML = ""
+function adicionar() {
+    let input = entrada.value;
+    if (input.trim() === "") {
+        alert("Escreva algo!");
+        return;
+    }
+    
+    
+    let item = '<li onclick="this.classList.toggle(\'feito\'); atualizarStorage()">'+ input + '</li>';
+    lista.innerHTML += item;
+    
+    entrada.value = "";
+    atualizarStorage(); 
+}
+
+function limpar() {
+    lista.innerHTML = "";
+    localStorage.removeItem('minhaListaHTML'); 
 }
